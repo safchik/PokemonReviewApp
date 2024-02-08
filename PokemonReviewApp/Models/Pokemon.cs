484d934f79554de194d5f6e27c1882371f193ac8
Dmytro <sagchik9@gmial.com>
2024-02-08 19:02:17 +0000

    many to many relations finished for models

--- PokemonReviewApp/Models/Pokemon.cs
+++ PokemonReviewApp/Models/Pokemon.cs
@@ -7,5 +7,7 @@
         public DateTime BirthDate { get; set; }
 
         public ICollection<Review> Reviews { get; set; }
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
+        public ICollection<PokemonCategory> PokemonCategories { get; set; }
     }
 }
