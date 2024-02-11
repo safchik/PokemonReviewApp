1613dca133ba80f27506fd6fe51011d121c5e864
Dmytro <sagchik9@gmial.com>
2024-02-11 15:53:48 +0000

    get request logic for pokemon id rating to poke controller added

--- PokemonReviewApp/Controllers/PokemonController.cs
+++ PokemonReviewApp/Controllers/PokemonController.cs
@@ -52,5 +52,25 @@ namespace PokemonReviewApp.Controllers
             }
             return Ok(pokemon);
         }
+
+        [HttpGet("{pokeId}/rating")]
+        [ProducesResponseType(200, Type = typeof(decimal))]
+        [ProducesResponseType(400)]
+
+        public IActionResult GetPokemonRating(int pokeId)
+        {
+            if (!_pokemonRepository.PokemonExists(pokeId))
+            {
+                return NotFound();
+            }
+
+            var rating = _pokemonRepository.GetPokemonRating(pokeId);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            return Ok(rating);
+        }
     }
 }
