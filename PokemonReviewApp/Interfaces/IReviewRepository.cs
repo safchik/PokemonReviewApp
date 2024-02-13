14cd6442c88e376ad945b45411c59a05d6d2fde0
Dmytro <sagchik9@gmial.com>
2024-02-13 17:35:02 +0000

    finished create request for review repository

--- PokemonReviewApp/Interfaces/IReviewRepository.cs
+++ PokemonReviewApp/Interfaces/IReviewRepository.cs
@@ -9,5 +9,7 @@ namespace PokemonReviewApp.Interfaces
 
         ICollection<Review> GetReviewsOfAPokemon(int pokeId);
         bool ReviewExists(int reviewId);
+        bool CreateReview(Review review);
+        bool Save();
     }
 }
